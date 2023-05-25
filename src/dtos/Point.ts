12f7d0e91fb48e1f8a3d7e5b3ae2faea07a3e2a8
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-05-25 10:30:53 +0200

    feat: add DrawPoint

--- src/dtos/Point.ts
+++ src/dtos/Point.ts
@@ -1,7 +1,11 @@
 export interface Point {
+    color: string;
+    initialX: number;
+    initialY: number;
     x1: number;
     y1: number;
+    initialX2: number;
+    initialY2: number;
     x2: number;
-    y2: number; 
-    color: any;
+    y2: number;
     }
